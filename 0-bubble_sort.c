0d52f767c9db45d24e0db16b07b14ff394e757ca
alzheimeer <fogniebla@hotmail.com>
2019-10-28 12:12:35 -0700

    fix big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,15 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t j;
 	int temp,  flag;
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < (size - 1); i++)
+	for (; flag; )
 	{
 		flag = 0;
-		for (j = 0; j < (size - i - 1); j++)
+		for (j = 0; j < (size - 1); j++)
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
@@ -24,7 +24,5 @@ void bubble_sort(int *array, size_t size)
 				flag = 1;
 				print_array(array, size);
 			}
-		if (flag == 0)
-			break;
 	}
 }
