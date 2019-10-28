cae507d0cf438a42872d4440168e7ee54ff7c415
alzheimeer <fogniebla@hotmail.com>
2019-10-27 20:56:32 -0700

    fix  bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,14 +8,23 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp,  flag;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < (size - 1); i++)
+	{
+		flag = 0;
 		for (j = 0; j < (size - i - 1); j++)
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
 			}
+		if (flag == 0)
+			break;
+	}
 }
