1166416ccdbb4b30802a3327d07955d092077840
alzheimeer <fogniebla@hotmail.com>
2019-10-28 22:17:33 -0700

    fix task 1 insertion sorted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,22 +3,22 @@
 /**
  * inter_nodes -  Swap two nodes in a listint_t doble linked list
  * @h: A pointer to the head of the double linked list
- * @n1: A pointer to the first node to swap
- * @n2: The second node to swap
+ * @a: A pointer to the first node to swap
+ * @b: The second node to swap
  */
-void inter_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void inter_nodes(listint_t **h, listint_t *a, listint_t *b)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	if (b->next)
+		b->next->prev = a;
+	if (a->prev)
+		a->prev->next = b;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*h = b;
+	a->next = b->next;
+	b->prev = a->prev;
+	b->next = a;
+	a->prev = b;
+	a = b->prev;
 }
 
 /**
@@ -28,18 +28,17 @@ void inter_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *i, *tmp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (i = (*list)->next; i; i = tmp)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		tmp = i->next;
+		while (i->prev && i->n < i->prev->n)
 		{
-			inter_nodes(list, &insert, iter);
+			inter_nodes(list, i->prev, i);
 			print_list((const listint_t *)*list);
 		}
 	}
